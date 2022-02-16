9dc696874313d2e8c35db2fb755cd2a2d20da42f
kts2442 <kts1006@naver.com>
2022-02-16 16:51:09 +0900

    travis CI added, AWS deploy

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
 const pool = mysql.createPool({
   connectionLimit: 15,
-  host: "mysql",
-  user: "root",
-  password: "kts1004",
-  database: "myapp",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 });
 
 exports.pool = pool;
